31fc4693e908711fa963af287ce959f98cb230bb
Max <maxjapri@gmail.com>
2025-04-07 14:16:39 +0700

    login page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,19 @@
-import { Button } from "@/components/ui/button"
- 
-export default function Home() {
+import { Webhook } from "lucide-react"
+
+import { LoginForm } from "@/components/login-form"
+
+export default function LoginPage() {
   return (
-    <div>
-      <Button>Click me</Button>
+    <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-muted p-6 md:p-10">
+      <div className="flex w-full max-w-sm flex-col gap-6">
+        <a href="#" className="flex items-center gap-2 self-center font-medium">
+          <div className="flex h-6 w-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
+            <Webhook className="size-4" />
+          </div>
+          Skripsi TeleScraper
+        </a>
+        <LoginForm />
+      </div>
     </div>
   )
 }
