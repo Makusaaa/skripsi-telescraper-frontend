fc624007068bb927df814cfd247f86b44112f7b3
Max <maxjapri@gmail.com>
2025-05-05 18:01:23 +0700

    remove unused template

--- src/app/(dashboard)/manage-alarm/components/mail.tsx
+++ src/app/(dashboard)/manage-alarm/components/mail.tsx
@@ -2,11 +2,6 @@
 
 import * as React from "react"
 import {
-  Search,
-} from "lucide-react"
-
-import { Input } from "@/components/ui/input"
-import {
   ResizableHandle,
   ResizablePanel,
   ResizablePanelGroup,
@@ -52,32 +47,24 @@ export function Mail({
           maxSize={40}
           >
           <Tabs defaultValue="all">
-            <div className="flex items-center px-4 py-2">
-              <h1 className="text-xl font-bold">Alarm Management</h1>
-              <TabsList className="ml-auto">
-                <TabsTrigger
-                  value="all"
-                  className="text-zinc-600 dark:text-zinc-200"
-                >
-                  All mail
-                </TabsTrigger>
-                <TabsTrigger
-                  value="unread"
-                  className="text-zinc-600 dark:text-zinc-200"
-                >
-                  Unread
-                </TabsTrigger>
-              </TabsList>
+            <div className="flex items-center px-4 pt-3 pb-1">
+              <h1 className="text-xl font-bold">Alarms</h1>
+                <TabsList className="ml-auto">
+                  <TabsTrigger
+                    value="all"
+                    className="text-zinc-600 dark:text-zinc-200"
+                  >
+                    All Alarms
+                  </TabsTrigger>
+                  <TabsTrigger
+                    value="unread"
+                    className="text-zinc-600 dark:text-zinc-200"
+                  >
+                    Handled Alarms
+                  </TabsTrigger>
+                </TabsList>
             </div>
             <Separator />
-            <div className="bg-background/95 p-4 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-              <form>
-                <div className="relative">
-                  <Search className="absolute left-2 top-2.5 h-4 w-4 text-muted-foreground" />
-                  <Input placeholder="Search" className="pl-8" />
-                </div>
-              </form>
-            </div>
             <TabsContent value="all" className="m-0">
               <MailList items={mails} />
             </TabsContent>
