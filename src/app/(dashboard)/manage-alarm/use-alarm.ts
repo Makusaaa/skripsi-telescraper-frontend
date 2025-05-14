09ddc0bc5ea43db05c04561bbaf22b67d692b95c
Max <maxjapri@gmail.com>
2025-05-14 16:34:23 +0700

    remove unused code

--- src/app/(dashboard)/manage-alarm/use-alarm.ts
+++ src/app/(dashboard)/manage-alarm/use-alarm.ts
@@ -1,15 +1,16 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { atom, useAtom } from "jotai"
 
-import { Alarm, alarms } from "./data"
-
 type Config = {
-  selected: Alarm["id"] | null
+  selected: any["id"] | null
 }
 
 const configAtom = atom<Config>({
-  selected: alarms[0].id,
+  selected: null,
 })
 
 export function useAlarm() {
   return useAtom(configAtom)
 }
+
+export const userAtom = atom([])
