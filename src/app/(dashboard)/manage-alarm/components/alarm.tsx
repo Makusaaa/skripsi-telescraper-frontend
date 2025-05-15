2d36b501815a13e3ff67baa927553df467243367
Max <maxjapri@gmail.com>
2025-05-15 17:31:09 +0700

    fix default size

--- src/app/(dashboard)/manage-alarm/components/alarm.tsx
+++ src/app/(dashboard)/manage-alarm/components/alarm.tsx
@@ -14,7 +14,6 @@ import {
   TabsList,
   TabsTrigger,
 } from "@/components/ui/tabs"
-import { TooltipProvider } from "@/components/ui/tooltip"
 import { MailDisplay } from "./alarm-display"
 import { AlarmList } from "./alarm-list"
 import { alarmListAtom, useAlarm } from "../use-alarm"
@@ -27,12 +26,10 @@ import { Session } from "@/lib/apiclient"
 import { useAtom } from "jotai"
 
 interface AlarmProps {
-  defaultLayout: number[] | undefined
   session: Session
 }
 
 export function AlarmsPage({
-  defaultLayout = [20, 32, 48],
   session
 }: AlarmProps) {
   const [alarm] = useAlarm()
@@ -61,74 +58,75 @@ export function AlarmsPage({
 	}, [setAlarms]);
 
   return (
-    <TooltipProvider delayDuration={0}>
-      <ResizablePanelGroup
-        direction="horizontal"
-        onLayout={(sizes: number[]) => {
-          document.cookie = `react-resizable-panels:layout:mail=${JSON.stringify(
-            sizes
-          )}`
-        }}
-        className="h-full max-h-[800px] items-stretch"
+    <ResizablePanelGroup
+      direction="horizontal"
+      onLayout={(sizes: number[]) => {
+        document.cookie = `react-resizable-panels:layout:mail=${JSON.stringify(
+          sizes
+        )}`
+      }}
+      className="h-full max-h-[800px] items-stretch"
+    >
+      <ResizablePanel
+        defaultSize={30}
+        minSize={30}
+        maxSize={40}
+        >
+        <Tabs defaultValue="all">
+          <div className="flex items-center px-4 pt-3 pb-1">
+            <h1 className="text-xl font-bold">Alarms</h1>
+              <TabsList className="ml-auto">
+                <TabsTrigger
+                  value="all"
+                  className="text-zinc-600 dark:text-zinc-200"
+                >
+                  All
+                </TabsTrigger>
+                <TabsTrigger
+                  value="open"
+                  className="text-zinc-600 dark:text-zinc-200"
+                >
+                  Open
+                </TabsTrigger>
+                <TabsTrigger
+                  value="closed"
+                  className="text-zinc-600 dark:text-zinc-200"
+                >
+                  Closed
+                </TabsTrigger>
+                <TabsTrigger
+                  value="assigned"
+                  className="text-zinc-600 dark:text-zinc-200"
+                >
+                  Assigned to me
+                </TabsTrigger>
+              </TabsList>
+          </div>
+          <Separator />
+          {isLoading && <LoaderIcon className="animate-spin mx-auto mt-15" />}
+          <TabsContent value="all" className="m-0">
+            <AlarmList items={alarms} />
+          </TabsContent>
+          <TabsContent value="open" className="m-0">
+            <AlarmList items={alarms.filter((item) => item.status == StatusEnum.Open)} />
+          </TabsContent>
+          <TabsContent value="closed" className="m-0">
+            <AlarmList items={alarms.filter((item) => item.status == StatusEnum.Closed)} />
+          </TabsContent>
+          <TabsContent value="assigned" className="m-0">
+            <AlarmList items={alarms.filter((item) => item.assignto == session.userid)} />
+          </TabsContent>
+        </Tabs>
+      </ResizablePanel>
+      <ResizableHandle withHandle />
+      <ResizablePanel
+        defaultSize={70}
+        minSize={30}
       >
-        <ResizablePanel
-          defaultSize={defaultLayout[0]}
-          minSize={30}
-          maxSize={40}
-          >
-          <Tabs defaultValue="all">
-            <div className="flex items-center px-4 pt-3 pb-1">
-              <h1 className="text-xl font-bold">Alarms</h1>
-                <TabsList className="ml-auto">
-                  <TabsTrigger
-                    value="all"
-                    className="text-zinc-600 dark:text-zinc-200"
-                  >
-                    All
-                  </TabsTrigger>
-                  <TabsTrigger
-                    value="open"
-                    className="text-zinc-600 dark:text-zinc-200"
-                  >
-                    Open
-                  </TabsTrigger>
-                  <TabsTrigger
-                    value="closed"
-                    className="text-zinc-600 dark:text-zinc-200"
-                  >
-                    Closed
-                  </TabsTrigger>
-                  <TabsTrigger
-                    value="assigned"
-                    className="text-zinc-600 dark:text-zinc-200"
-                  >
-                    Assigned to me
-                  </TabsTrigger>
-                </TabsList>
-            </div>
-            <Separator />
-            {isLoading && <LoaderIcon className="animate-spin mx-auto mt-15" />}
-            <TabsContent value="all" className="m-0">
-              <AlarmList items={alarms} />
-            </TabsContent>
-            <TabsContent value="open" className="m-0">
-              <AlarmList items={alarms.filter((item) => item.status == StatusEnum.Open)} />
-            </TabsContent>
-            <TabsContent value="closed" className="m-0">
-              <AlarmList items={alarms.filter((item) => item.status == StatusEnum.Closed)} />
-            </TabsContent>
-            <TabsContent value="assigned" className="m-0">
-              <AlarmList items={alarms.filter((item) => item.assignto == session.userid)} />
-            </TabsContent>
-          </Tabs>
-        </ResizablePanel>
-        <ResizableHandle withHandle />
-        <ResizablePanel defaultSize={defaultLayout[2]} minSize={30}>
-          <MailDisplay
-            alarm={alarms.find((item) => item.alarmid === alarm.selected) || null}
-          />
-        </ResizablePanel>
-      </ResizablePanelGroup>
-    </TooltipProvider>
+        <MailDisplay
+          alarm={alarms.find((item) => item.alarmid === alarm.selected) || null}
+        />
+      </ResizablePanel>
+    </ResizablePanelGroup>
   )
 }
--- src/app/(dashboard)/manage-alarm/page.tsx
+++ src/app/(dashboard)/manage-alarm/page.tsx
@@ -1,17 +1,13 @@
 "use server"
-import { cookies } from "next/headers"
 import { AlarmsPage } from "./components/alarm"
 import { getSession } from "@/lib/apiclient"
 
 export default async function ManageAlarmPage() {
-  const layout = (await cookies()).get("react-resizable-panels:layout:mail")
-  const defaultLayout = layout ? JSON.parse(layout.value) : undefined
   const session = await getSession();
   return (
     <>
       <div className="hidden flex-col md:flex">
         <AlarmsPage
-          defaultLayout={defaultLayout}
           session={session}
         />
       </div>
